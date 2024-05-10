a9ade2592c18166d6a0a8439b9c85a1dffeacf18
Conank <ngomanh.dev@gmail.com>
2024-05-10 12:42:28 +0700

    a

--- app/src/main/java/com/example/testprogram/MainActivity.kt
+++ app/src/main/java/com/example/testprogram/MainActivity.kt
@@ -25,7 +25,6 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
